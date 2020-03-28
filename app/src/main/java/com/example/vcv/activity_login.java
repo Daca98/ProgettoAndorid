88f6ace00824df90fd04bb4e86caeca267fa6e93
Andrea Dalle Fratte <dallefratte.andre@gmail.com>
2020-03-28 18:26:33 +0100

    added fragments for login and signin

--- app/src/main/java/com/example/vcv/activity_login.java
+++ app/src/main/java/com/example/vcv/activity_login.java
@@ -1,14 +1,92 @@
 package com.example.vcv;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.example.vcv.formlogin.Login;
+import com.example.vcv.formlogin.Signin;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class activity_login extends AppCompatActivity {
+
+    private android.app.FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        fragmentManager = getFragmentManager();
+
+        Fragment fragment = new Login();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.body_login_signin_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+    // Handler
+    public void clickLogin(View view) {
+        Button BLogin = findViewById(R.id.b_login);
+        Button BSignin = findViewById(R.id.b_signin);
+
+        BLogin.setBackgroundColor(getResources().getColor(R.color.white));
+        BSignin.setBackgroundColor(getResources().getColor(R.color.bck_tab_unselected));
+        insertFragmentLogin();
+    }
+
+    public void clickSignin(View view) {
+        Button BLogin = findViewById(R.id.b_login);
+        Button BSignin = findViewById(R.id.b_signin);
+
+        BLogin.setBackgroundColor(getResources().getColor(R.color.bck_tab_unselected));
+        BSignin.setBackgroundColor(getResources().getColor(R.color.white));
+        insertFragmentSignin();
+    }
+
+    // Utility
+    private void insertFragmentLogin() {
+        LinearLayout containerFragment = findViewById(R.id.container_fragment);
+        containerFragment.setPadding(15, 10, 15, 70);
+
+        fragmentManager = getFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.body_login_signin_fragment);
+
+        if (fragment != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }
+
+        Fragment newFragment = new Login();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.body_login_signin_fragment, newFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void insertFragmentSignin() {
+        LinearLayout containerFragment = findViewById(R.id.container_fragment);
+        containerFragment.setPadding(15, 10, 15, 35);
+
+        fragmentManager = getFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.body_login_signin_fragment);
+
+        if (fragment != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }
+
+        Fragment newFragment = new Signin();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.body_login_signin_fragment, newFragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/vcv/formlogin/Signin.java
@@ -0,0 +1,18 @@
+package com.example.vcv.formlogin;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vcv.R;
+
+public class Signin extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signin, container, false);
+    }
+}
