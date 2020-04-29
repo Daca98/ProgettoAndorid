6d75fa5a824b950e039599018164588be0c1cd2e
mattia <mat.daca@gmail.com>
2020-04-29 18:11:43 +0200

    Map Fragment creation

--- app/src/main/java/com/example/vcv/ui/path/PathFragment.java
+++ app/src/main/java/com/example/vcv/ui/path/PathFragment.java
@@ -9,27 +9,44 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.vcv.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PathFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathFragment extends FragmentActivity implements OnMapReadyCallback{
 
     private PathViewModel pathViewModel;
+    private GoogleMap map;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         pathViewModel =
                 ViewModelProviders.of(this).get(PathViewModel.class);
         View root = inflater.inflate(R.layout.fragment_path, container, false);
-        final TextView textView = root.findViewById(R.id.text_path);
-        pathViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
     }
 }
