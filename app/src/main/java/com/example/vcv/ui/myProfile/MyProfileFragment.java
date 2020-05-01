2720b7a6d5841d1745c8d5ee16219f81b2fe2e0f
Andrea Dalle Fratte <dallefratte.andre@gmail.com>
2020-05-01 13:59:25 +0200

    Add permissions to upload image

--- app/src/main/java/com/example/vcv/ui/myProfile/MyProfileFragment.java
+++ app/src/main/java/com/example/vcv/ui/myProfile/MyProfileFragment.java
@@ -1,15 +1,16 @@
 package com.example.vcv.ui.myProfile;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +20,9 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.vcv.ForgotPasswordActivity;
 import com.example.vcv.R;
@@ -35,14 +37,11 @@ import static android.app.Activity.RESULT_OK;
 
 public class MyProfileFragment extends Fragment {
 
-    private MyProfileViewModel myProfileViewModel;
     private static int RESULT_LOAD_IMAGE = 1;
     private static final String DIR_IMAGE_NAME = "imageDir";
+    private static final int MY_PERMISSIONS_CODE = 1;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        myProfileViewModel =
-                ViewModelProviders.of(this).get(MyProfileViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_myprofile, container, false);
 
         Button b_forgetPassword = root.findViewById(R.id.b_change_password);
@@ -58,11 +57,14 @@ public class MyProfileFragment extends Fragment {
         b_editProfilePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
+                // Check if there are permissions.
+                // TRUE -> There are no permission about READ_EXTERNAL_STORAGE
+                // FALSE -> Permissions have already been setted
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_CODE); // MY_PERMISSIONS_CODE is a value that we can define ourself in this class
+                } else {
+                    openGalleryAndChooseImage();
+                }
             }
         });
 
@@ -98,12 +100,7 @@ public class MyProfileFragment extends Fragment {
                 String picturePath = cursor.getString(columnIndex);
                 cursor.close();
 
-                ImageView profilePicture = getActivity().findViewById(R.id.profile_picture);
-                Bitmap bitmapImage = BitmapFactory.decodeFile(picturePath);
-                profilePicture.setImageBitmap(bitmapImage);
-                profilePicture.setBackgroundResource(0);
-
-                saveToInternalStorage(bitmapImage);
+                saveToInternalStorage(BitmapFactory.decodeFile(picturePath));
 
             } catch (Exception e) {
                 Toast.makeText(getContext(), getString(R.string.can_not_load_image), Toast.LENGTH_SHORT).show();
@@ -113,15 +110,31 @@ public class MyProfileFragment extends Fragment {
     }
 
     // Utility
+    private void openGalleryAndChooseImage() {
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    private void setDefaultIcon() {
+        ImageView profilePicture = getActivity().findViewById(R.id.profile_picture);
+        profilePicture.setImageBitmap(null); // remove uploaded picture
+        profilePicture.setBackgroundResource(R.drawable.user);
+        Button buttonClear = getActivity().findViewById(R.id.b_remove_profile_picture);
+        buttonClear.setVisibility(View.INVISIBLE);
+    }
+
     private boolean saveToInternalStorage(Bitmap bitmapImage) {
         boolean saveOk = false;
         FileOutputStream fos = null;
 
         try {
             ContextWrapper cw = new ContextWrapper(getActivity().getApplicationContext());
-            // path to /data/data/yourapp/app_data/ + dirName
+            // path to /data/data/yourapp/app_data/ + DIR_IMAGE_NAME
             File directory = cw.getDir(DIR_IMAGE_NAME, Context.MODE_PRIVATE);
-            // Creo imageDir
+            // Create a directory with the name specified in DIR_IMAGE_NAME
             File mypath = new File(directory, "profile.jpg");
 
             fos = new FileOutputStream(mypath);
@@ -152,6 +165,7 @@ public class MyProfileFragment extends Fragment {
                 Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
                 if (b != null) {
                     ImageView profilePicture = this.getActivity().findViewById(R.id.profile_picture);
+                    profilePicture.setBackgroundResource(0); // Remove the default icon
                     profilePicture.setImageBitmap(b);
                     Button buttonClear = getActivity().findViewById(R.id.b_remove_profile_picture);
                     buttonClear.setVisibility(View.VISIBLE);
@@ -163,10 +177,7 @@ public class MyProfileFragment extends Fragment {
         }
 
         if (hasToSetDefaultImage) {
-            ImageView profilePicture = getActivity().findViewById(R.id.profile_picture);
-            profilePicture.setBackgroundResource(R.drawable.user);
-            Button buttonClear = getActivity().findViewById(R.id.b_remove_profile_picture);
-            buttonClear.setVisibility(View.INVISIBLE);
+            setDefaultIcon();
         }
     }
 
@@ -179,11 +190,7 @@ public class MyProfileFragment extends Fragment {
             if (f.delete()) {
                 Toast.makeText(getContext(), getString(R.string.success_remove_image), Toast.LENGTH_SHORT).show();
 
-                ImageView profilePicture = getActivity().findViewById(R.id.profile_picture);
-                profilePicture.setImageBitmap(null);
-                profilePicture.setBackgroundResource(R.drawable.user);
-                Button buttonClear = getActivity().findViewById(R.id.b_remove_profile_picture);
-                buttonClear.setVisibility(View.INVISIBLE);
+                setDefaultIcon();
             } else {
                 Toast.makeText(getContext(), getString(R.string.error_remove_image), Toast.LENGTH_SHORT).show();
             }
